5cfc635c89246bf32f17f2927bc1406c7f3b1d92
Mo Bitar <me@mbbitar.com>
2013-03-26 11:16:27 -0500

    updates

--- Categories/NSTimer+Blocks.h
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface NSTimer (Blocks)
-+ (id)scheduledTimerWithTimeInterval:(NSTimeInterval)inTimeInterval block:(void (^)())inBlock repeats:(BOOL)inRepeats;
-+ (id)timerWithTimeInterval:(NSTimeInterval)inTimeInterval block:(void (^)())inBlock repeats:(BOOL)inRepeats;
-@end
